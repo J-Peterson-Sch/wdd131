d1e07dd1e642e2f36d8022413cf73a5f296b0bca
Jordan Peterson <peterson.jordan.mark@gmail.com>
2025-03-04 17:03:40 -0700

    Fixed a misunderstanding about the last modified date.

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -2,7 +2,7 @@ const copywriteText = document.querySelector("#currentyear");
 const lastModifiedText = document.querySelector("#lastModified");
 
 const currentYear = new Date().getFullYear();
-const currentDate = new Date().toLocaleString();
+const lastModifiedDate = document.lastModified;
 
 copywriteText.innerHTML = `&copy;${currentYear}<span > Jordan Peterson USA</span>`;
-lastModifiedText.innerHTML = `Last Modification: ${currentDate}`;
+lastModifiedText.innerHTML = `Last Modification: ${lastModifiedDate}`;
