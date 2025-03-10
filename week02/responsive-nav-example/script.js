30a66aef1d287b12f13ba11f87f20e8dd8c729f0
Jordan Peterson <peterson.jordan.mark@gmail.com>
2025-03-09 18:34:45 -0600

    Added responsive nav example page in week 2.

---
+++ week02/responsive-nav-example/script.js
@@ -0,0 +1,10 @@
+
+window.addEventListener('DOMContentLoaded',(event) =>{
+    const hamButton = document.querySelector("#menu");
+    const navigation = document.querySelector(".navigation");
+
+    hamButton.addEventListener("click", () => {
+	navigation.classList.toggle("open");
+	hamButton.classList.toggle("open");
+    });
+})
