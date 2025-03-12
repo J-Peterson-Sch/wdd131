08ad41f328bc8fe9fec216ea649e3288d17e8914
Jordan Peterson <peterson.jordan.mark@gmail.com>
2025-03-12 11:08:46 -0600

    Fixes for the mobile nav.

--- scripts/temples.js
+++ scripts/temples.js
@@ -2,6 +2,8 @@ const hamButton = document.querySelector("#menu");
 const navigation = document.querySelector(".navigation");
 const header = document.querySelector("header");
 
+
+//Event listener to toggle the open class when you click the button
 hamButton.addEventListener("click", () => {
 navigation.classList.toggle("open");
 hamButton.classList.toggle("open");
