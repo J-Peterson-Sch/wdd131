c32c87ec27d0023ce2cd4995beccbf05c8290c8a
Jordan Peterson <peterson.jordan.mark@gmail.com>
2025-03-13 16:32:09 -0600

    Changed the js to check if the weather conditions justify a wind chill factor.

--- scripts/place.js
+++ scripts/place.js
@@ -1,14 +1,15 @@
-const temp = 20;
+const temp = 10;
 const windSpeed = 5;
 const windChillElement = document.querySelector("#wind-chill");
 
 
 function calculateWindChill(temp, windSpeed) {
-    const windChill = 13.12 + (0.6215 * temp) - (11.37 * windSpeed**0.16) + (0.3965 * temp * windSpeed**0.16);
-    return Math.round(windChill);
+    return Math.round(13.12 + (0.6215 * temp) - (11.37 * windSpeed**0.16) + (0.3965 * temp * windSpeed**0.16));
 }   
 
-if (temp !== "" & windSpeed !=="") {
+if (temp <= 10 & windSpeed > 4.8) {
     const windChill = calculateWindChill(temp,windSpeed);
     windChillElement.innerHTML = `${windChill}&#176;C`;
+}else{
+    windChillElement.innerHTML = `NA`;
 }
