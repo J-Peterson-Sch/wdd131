d93f7a11fa68519cce9a13aaaa578980b75c5054
Jordan Peterson <peterson.jordan.mark@gmail.com>
2025-03-13 16:32:50 -0600

    White space change.

--- scripts/place.js
+++ scripts/place.js
@@ -2,7 +2,6 @@ const temp = 10;
 const windSpeed = 5;
 const windChillElement = document.querySelector("#wind-chill");
 
-
 function calculateWindChill(temp, windSpeed) {
     return Math.round(13.12 + (0.6215 * temp) - (11.37 * windSpeed**0.16) + (0.3965 * temp * windSpeed**0.16));
 }   
