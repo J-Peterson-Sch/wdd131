377c972ca58ebfab544b68b0fa7ca0779f82c188
Jordan Peterson <peterson.jordan.mark@gmail.com>
2025-04-08 22:06:42 -0600

    Inital work for final project site.

---
+++ project/scripts/getdates.js
@@ -0,0 +1,8 @@
+const copywriteText = document.querySelector("#currentyear");
+const lastModifiedText = document.querySelector("#lastModified");
+
+const currentYear = new Date().getFullYear();
+const lastModifiedDate = document.lastModified;
+
+copywriteText.innerHTML = `&copy;${currentYear}<span > Jordan Peterson USA</span>`;
+lastModifiedText.innerHTML = `Last Modification: ${lastModifiedDate}`;
