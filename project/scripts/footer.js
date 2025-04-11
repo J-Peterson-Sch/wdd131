16ada6dcba7aae13b04f795a1469a2271d5e69cc
Jordan Peterson <peterson.jordan.mark@gmail.com>
2025-04-10 19:34:52 -0600

    Lots of progress.

--- project/scripts/footer.js
+++ project/scripts/footer.js
@@ -4,5 +4,5 @@ const lastModifiedText = document.querySelector("#lastModified");
 const currentYear = new Date().getFullYear();
 const lastModifiedDate = document.lastModified;
 
-copywriteText.innerHTML = `&copy;${currentYear}<span > Jordan Peterson USA</span>`;
+copywriteText.innerHTML = `&copy;${currentYear}<span > Jordan Peterson - USA</span>`;
 lastModifiedText.innerHTML = `Last Modification: ${lastModifiedDate}`;
