3c44aa3dd9e5b6836be51954c2becc4389118b5e
Jordan Peterson <peterson.jordan.mark@gmail.com>
2025-04-12 12:27:49 -0600

    Lots of progress. Finished JS files.

--- project/scripts/dough-calculator.js
+++ project/scripts/dough-calculator.js
@@ -1,42 +1,64 @@
+const doughBalls = [
+    {   
+        size:"Personal",
+        diameter: 10,
+        weight:175
+    },
+    {   
+        size:"Small",
+        diameter: 12,
+        weight:250
+    },
+    {
+        size:"Medium",
+        diameter: 14,
+        weight:350
+    },
+    {
+        size:"Large",
+        diameter: 16,
+        weight:450
+    },
+    {
+        size:"Extra Large",
+        diameter: 18,
+        weight:580
+    }
+]
+
+const crustDiaDropDown = document.getElementById('crust-diameter');
+
+function populateDropDown(doughBalls) {
+
+    crustDiaDropDown.innerHTML = "";
+
+    doughBalls.forEach(element => { // <=== This is an array method
+        const option = document.createElement('option');
+        option.text = `${element.size} - ${element.diameter} inches`;
+        option.value = element.weight;
+        crustDiaDropDown.add(option);
+    });
+}
+
 function updateIngredientQuantities(event) {
-    // Prevent the default form submission
     event.preventDefault();
     calculateQuantities();
 }
 
 function calculateQuantities() {
     const numCrusts = parseInt(document.getElementById('numCrusts').value);
-    const diameter = parseInt(document.getElementById('diameter').value);
+    const doughWeight = parseInt(document.getElementById('crust-diameter').value);
     
-    let doughWeight;
-    if (diameter === 10) {
-        doughWeight = 175
-    }
-    else if (diameter === 12){
-        doughWeight = 250
-    }
-    else if (diameter === 14){
-        doughWeight = 350
-    }
-    else if (diameter === 16){
-        doughWeight = 350
-    }
-    
-    
-    // Calculate total dough needed
     const totalDough = numCrusts * doughWeight;
     
-    // Proportionate ingredient amounts based on a total dough required for the recipe (1470g)
     const ratio = totalDough / 2506; 
     
-    // Calculate ingredients based on ratio
     const apFlour = 735 * ratio;
     const ooFlour = 735 * ratio;
     const water = 1000 * ratio;
     const salt = 30 * ratio;
     const yeast = 6 * ratio;
     
-    // Display results
     document.getElementById('apFlour').textContent = `${apFlour.toFixed(0)}g All Purpose Flour`;
     document.getElementById('ooFlour').textContent = `${ooFlour.toFixed(0)}g OO Flour`;
     document.getElementById('water').textContent = `${water.toFixed(0)}g Water`;
@@ -46,4 +68,5 @@ function calculateQuantities() {
     document.getElementById('results').style.display = 'block';
 }
 
+populateDropDown(doughBalls);
 calculateQuantities();
